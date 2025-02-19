a384785c9b6aeeb33f8de2ee35e22104e06cc0b0
Wai Ho Fan <waihofan.419@gmail.com>
2025-02-19 09:53:24 +0800

    Initial Commit

---
+++ StreamingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StreamingApp
+//
+//  Created by Gary on 19/2/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StreamingApp/StreamingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamingAppApp.swift
+//  StreamingApp
+//
+//  Created by Gary on 19/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct StreamingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
