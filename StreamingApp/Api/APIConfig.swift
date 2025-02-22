c0310e91fdc807ea41495aed04ad83cc1b2c26f3
Wai Ho Fan <waihofan.419@gmail.com>
2025-02-22 22:13:32 +0800

    Added APIConfig, fetchTrendShows api

---
+++ StreamingApp/Api/APIConfig.swift
@@ -0,0 +1,14 @@
+//
+//  APIConfig.swift
+//  StreamingApp
+//
+//  Created by Gary on 22/2/2025.
+//
+
+import Foundation
+
+struct APIConfig {
+    static let baseURL = "https://streaming-availability.p.rapidapi.com"
+    static let apiKey = "8d90e19104msh470d211a8eb2ce1p125799jsn9989d06cb713"
+    static let apiHost = "streaming-availability.p.rapidapi.com"
+}
--- StreamingApp/Api/ApiClient.swift
+++ StreamingApp/Api/ApiClient.swift
@@ -8,6 +8,11 @@
 import Foundation
 
 struct ApiClient {
+    let headers = [
+        "x-rapidapi-key": APIConfig.apiKey,
+        "x-rapidapi-host": APIConfig.apiHost
+    ]
+    
     enum APIError: Error {
         case invalidResponse
         case decodeError(DecodingError)
@@ -25,13 +30,8 @@ struct ApiClient {
         }
     }
     
-    func fetchShow() async throws -> Show {
-        let headers = [
-            "x-rapidapi-key": "8d90e19104msh470d211a8eb2ce1p125799jsn9989d06cb713",
-            "x-rapidapi-host": "streaming-availability.p.rapidapi.com"
-        ]
-
-        let request = NSMutableURLRequest(url: NSURL(string: "https://streaming-availability.p.rapidapi.com/shows/tt0120338?series_granularity=episode&output_language=en")! as URL,
+    func fetchShow(imdbId: String) async throws -> Show {
+        let request = NSMutableURLRequest(url: NSURL(string: "https://streaming-availability.p.rapidapi.com/shows/\(imdbId)?series_granularity=episode&output_language=en")! as URL,
                                                 cachePolicy: .useProtocolCachePolicy,
                                             timeoutInterval: 10.0)
         request.httpMethod = "GET"
@@ -39,7 +39,7 @@ struct ApiClient {
 
         let session = URLSession.shared
         do {
-            // 3. 發送請求
+            // Request
             let (data, response) = try await session.data(for: request as URLRequest)
             
             if let jsonString = String(data: data, encoding: .utf8) {
@@ -71,6 +71,42 @@ struct ApiClient {
     }
     
     func fetchTrendShows() async throws -> [Show] {
-        return []
+        let request = NSMutableURLRequest(url: NSURL(string: "https://streaming-availability.p.rapidapi.com/shows/top?country=us&service=netflix")! as URL,
+                                                cachePolicy: .useProtocolCachePolicy,
+                                            timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+
+        let session = URLSession.shared
+        do {
+            // 3. 發送請求
+            let (data, response) = try await session.data(for: request as URLRequest)
+            
+            if let jsonString = String(data: data, encoding: .utf8) {
+                print("Response Trends shows: \(jsonString)")
+            }
+            
+            // 4. 驗證響應
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                throw APIError.invalidResponse
+            }
+            
+            // 5. 解碼數據
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            
+            do {
+                let shows = try decoder.decode([Show].self, from: data)
+                print("Successfully decoded shows - count \(shows.count)")
+                return shows
+            } catch let decodingError as DecodingError {
+                throw APIError.decodeError(decodingError)
+            }
+        } catch let networkError as APIError {
+            throw networkError
+        } catch {
+            throw APIError.networkError(error)
+        }
     }
 }
